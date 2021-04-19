b4d904be1485390af27ab444f21e8cf57867bfbd
adamszaruga <adamszaruga@users.noreply.github.com>
2021-04-19 03:03:04 +0000

    deploy: 8c3cadf2058bbed751e4c92bfaa3813de12de26a

---
+++ _next/static/BAqqXTwanfodkrsOyT5mD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-e9e847bbbccda5c447b5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0f3d9ad6c3bb705ecda3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
