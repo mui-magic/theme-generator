930a3b0cbbc0390439c92b7cb4f379e578a92dc5
adamszaruga <adamszaruga@users.noreply.github.com>
2021-08-09 00:00:56 +0000

    deploy: 647420ec32663205c0a7c5e0fb631514cff556aa

---
+++ _next/static/j-rasvsRdp8aNlCTFXnfm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/104-a982ab56f09ec63ae93f.js","static/chunks/pages/index-6d308373c2a0860cbf4b.js"],"/_error":["static/chunks/pages/_error-e69890b6db18dcbc6fa4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
